896cc20afeb59862eb3851e0460c900bc44c5eab
Max-Gabriel Susman <gabe.susman@gmail.com>
2026-08-16 08:43:49 -0600

    began implementing the dataset relay node

--- src/dataset_relay_node.cpp
+++ src/dataset_relay_node.cpp
@@ -1,1 +1,17 @@
+#include <algorithm>
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "argus_core/argus_wire.h"
 #include "argus_sim/replay_server.hpp"
+
+namespace argus_sim
+{
+    class DatasetRelayNode : public rclcpp::Node
+    {
+        // needs impl
+    }
+}
